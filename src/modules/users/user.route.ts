aa0f796c713f9b23fbfcd5a853a48f76e5e40d08
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2023-12-03 09:36:25 +0600

    done from office

---
+++ src/modules/users/user.route.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+router.get('/',)
+router.get('/:id')
+router.post("/",)
+router.delete('/:id');
+router.put('/:id',);
+
+export const userRouter = router
