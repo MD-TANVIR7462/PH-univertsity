285852a79007e06504ee65613626ca9ba0fef45d
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2023-12-03 09:51:55 +0600

    user model creatUser controler done

---
+++ src/modules/users/user.controler.ts
@@ -0,0 +1,23 @@
+import { Request } from "express";
+import { USerServices } from "./userServices";
+
+
+const createUserCl = async (req: Request, res: Response) => {
+    try {
+      const userData = req.body;
+      const result = await USerServices.creatUser(userData)
+      res.status(200).json({
+        success: true,
+        message: "User created successfully",
+        data: result,
+      });
+    } catch (err) {
+     
+      console.error(err);
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        error: err
+      });
+    }
+  };
---
+++ src/modules/users/userServices.ts
@@ -0,0 +1,39 @@
+import { Tuser } from "./user.interface";
+import { UserModel } from "./user.model";
+
+const creatUser = async (data: Tuser) => {
+  const result = await UserModel.create(data);
+  return result;
+};
+
+const getUser = async (id: string) => {
+  const result = await UserModel.findOne({ _id: { $eq: id } });
+  return result;
+};
+
+const getSingleUser = async (id: string) => {
+    const result = await UserModel.findOne({ _id: { $eq: id } });
+    return result;
+}
+
+const deleteUser = async (id: string) => {
+  const result = await UserModel.deleteOne({ _id: id });
+  return result;
+};
+
+const updateUser = async (id: string, data: Partial<Tuser>) => {
+    const result = await UserModel.updateOne(
+      { _id: { $eq: id } },
+      { $set: data }
+    );
+    return result;
+}
+
+//export all the services
+export const USerServices = {
+    creatUser,
+    getUser,
+    getSingleUser,
+    deleteUser,
+    updateUser,
+}
